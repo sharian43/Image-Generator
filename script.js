aa208519729ded26ae104eadeabe71176fd58fe3
Sharian Johnson <114778798+sharian43@users.noreply.github.com>
2023-08-29 16:23:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "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";
+const apiKey = "YOUR_API_KEY";
 
 const maxImages = 4; // Number of images to generate for each prompt
 let selectedImageNumber = null;
